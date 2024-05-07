b2673a6124f192069fbbe3680fa8c5680882d718
Hasan <mr.k779@outlook.com>
2024-05-08 01:46:03 +0600

    Create database.ts

---
+++ src/lib/database.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const db = drizzle(connection);
+export { db };
