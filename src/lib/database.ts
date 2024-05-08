5292fefc100a0b317e7d27f32dd369ac73c263bf
Hasan <mr.k779@outlook.com>
2024-05-08 20:20:02 +0600

    Update database.ts

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -2,6 +2,8 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from '../../schema/schema';
 import { userTable } from '../../schema/schema';
+import { Logger as drizzleLogger } from 'drizzle-orm/logger';
+import { logger } from './logger';
 
 const connection = await mysql.createConnection({
   host: process.env.DB_HOST,
@@ -13,5 +15,11 @@ const connection = await mysql.createConnection({
 export type User = typeof userTable.$inferSelect;
 export type NewUser = typeof userTable.$inferInsert;
 
-const db = drizzle(connection, { schema: schema, mode: 'default' });
+class DBLogger implements drizzleLogger {
+  logQuery(query: string, params: unknown[]): void {
+    logger.debug({ query, params });
+  }
+}
+
+const db = drizzle(connection, { schema: schema, mode: 'default', logger: new DBLogger() });
 export { connection, db };
