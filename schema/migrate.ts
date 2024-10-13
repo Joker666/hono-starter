ecc8dff733aeb52cb89f06e477cea53a10e85e73
Rafi <mr.k779@outlook.com>
2024-10-13 00:25:25 -0300

    Update migrate command

--- schema/migrate.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { migrate } from 'drizzle-orm/mysql2/migrator';
-import { connection, db } from '../src/lib/database';
-
-// This will run migrations on the database, skipping the ones already applied
-await migrate(db, { migrationsFolder: './schema/migration' });
-
-// Don't forget to close the connection, otherwise the script will hang
-await connection.end();
