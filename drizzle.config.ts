9c66e3c9ecb4484832db374357f8ca8ae29b673b
Rafi <mr.k779@outlook.com>
2025-03-02 03:07:28 -0400

    Fix drizzle kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
-import env from './src/lib/env.js';
+import env from './src/lib/env.ts';
 
 export default defineConfig({
-  schema: '.src/db/schema/schema.ts',
-  out: '.src/db/schema/migration',
+  schema: './src/db/schema/schema.ts',
+  out: './src/db/schema/migration',
   dialect: 'mysql',
   dbCredentials: {
     host: env.DB_HOST,
